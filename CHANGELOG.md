3b8698ee95d3b4f60beaef61f1dd1b4a3f82121f
sviatoslav <svyat770@gmail.com>
2019-06-09 18:46:06 +0300

    Added ability to watch and send both users and chat messages lists for a freshly comnnected users.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 ## Chat-App
 
 
+# 2.0.0
+ * Added ability to watch and send both users and chat messages lists for a freshly comnnected users.
+ * Minor inprovements.
+
 # 1.1.0
  * Added Redux Logger for better testing.
  * Upgraded run command.
--- server/app.js
+++ server/app.js
@@ -1,12 +1,20 @@
+
+const debug = require('debug')('app:build:chat-server');
 const WebSocket = require('ws');
 const port = 8989 || process.env.PORT;
 const wss = new WebSocket.Server({ port });
 
+const generateUUID = require('../src/utils/uuid');
+
 const users = [];
+const messages = [];
+const INITIAL_DATA = 'INITIAL_DATA';
 const ADD_USER = 'ADD_USER';
 const ADD_MESSAGE = 'ADD_MESSAGE';
 const REMOVE_USER = 'REMOVE_USER';
 const USERS_LIST = 'USERS_LIST';
+
+debug('Chat App Server is Started!');
 
 const storeTools = {
   addNewUserToStore: newUser => {
@@ -14,15 +22,14 @@ const storeTools = {
   },
   deleteDisconnectedUser: userConnectionID => {
     users.splice(0, users.length, ...users.filter(user => user.uuid !== userConnectionID));
+  },
+  addNewMessageToStore: newMessage => {
+    messages.push(newMessage);
   }
 };
 
 const helpers = {
-  setConnectedUserID: () => {
-    const connectedUserID = Date.now() + Math.floor(Math.random() * (1000 - 1) + 1);
-
-    return connectedUserID;
-  }
+  setConnectedUserID: () => generateUUID()
 };
 
 const broadcast = (payload, ws) => {
@@ -45,7 +52,7 @@ const broadcastNotificationHandlers = {
       ws
     );
   },
-  updatedMessagesList: (payload = {}, ws) => {
+  updatedSubscribersMessageList: (payload = {}, ws) => {
     broadcast(
       {
         type: ADD_MESSAGE,
@@ -60,8 +67,9 @@ const webSocketEventsHandlers = {
   subscribeNewUser: (usersList = [], ws) => {
     ws.send(
       JSON.stringify({
-        type: USERS_LIST,
-        users: usersList
+        type: INITIAL_DATA,
+        users: usersList,
+        messages
       })
     );
   }
@@ -76,19 +84,29 @@ const eventsHanlders = {
 
     const newUser = {
       name,
-      id: users.length + 1,
       uuid: userConnectionID
     };
 
     addNewUserToStore(newUser);
     subscribeNewUser(users, ws);
     updatedSubscribersUserList(users, ws);
+    debug('New user is just connected:', newUser);
   },
-  [ADD_MESSAGE]: (payload, ws) => {
-    const { updatedMessagesList } = broadcastNotificationHandlers;
-    const { message = '', author = '' } = payload;
+  [ADD_MESSAGE]: (payload, ws, userConnectionID) => {
+    const { message = '', uuid = null, author = '' } = payload;
+    const { updatedSubscribersMessageList } = broadcastNotificationHandlers;
+    const { addNewMessageToStore } = storeTools;
 
-    updatedMessagesList({ message, author }, ws);
+    const newMessage = {
+      message,
+      uuid,
+      uuidAuthor: userConnectionID,
+      author
+    };
+
+    addNewMessageToStore(newMessage);
+    updatedSubscribersMessageList(newMessage, ws);
+    debug('New message is written:', newMessage, messages);
   },
   [REMOVE_USER]: (ws, userConnectionID) => {
     const { updatedSubscribersUserList } = broadcastNotificationHandlers;
@@ -96,6 +114,7 @@ const eventsHanlders = {
 
     deleteDisconnectedUser(userConnectionID);
     updatedSubscribersUserList(users, ws);
+    debug('Some user leave:', users.find(user => user.uuid === userConnectionID));
   }
 };
 
