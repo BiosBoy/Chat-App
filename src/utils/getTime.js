7ad3be5756ef8c22ecdac501e04bb205a8eb3086
sviatoslav <svyat770@gmail.com>
2019-06-16 19:08:24 +0300

    Fixed getTime function for zero hours.

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -1,6 +1,6 @@
 const getTimestamp = timestamp => {
   const date = new Date(timestamp);
-  const hours = date.getHours();
+  const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
   const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
 
   return `${hours}:${minutes}`;
