3068bc52a135a6d7fa1f9369e9854e6d7a6bf0f9
sviatoslav <svyat770@gmail.com>
2019-06-16 19:09:27 +0300

    Improved initialStore to make it more predictable.

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -2,10 +2,21 @@ const initialState = {
   currentUser: {
     name: '',
     uuid: null,
-    isDisconnected: false
+    connectionStatus: 'notConnected'
   },
-  users: [],
-  messages: []
+  debug: {
+    errorType: '',
+    errorMessage: '',
+    showDebug: false
+  },
+  users: {
+    list: [],
+    mobileLayout: false
+  },
+  messages: [],
+  common: {
+    disableBlurOverlay: false
+  }
 };
 
 export default initialState;
