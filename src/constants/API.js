21524a8effffb0819f4b84cdf14248297279d902
sviatoslav <svyat770@gmail.com>
2019-11-18 01:40:45 +0200

    Fixed CORS issue for https requests

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,4 +1,4 @@
 const WSS_END_POINT = __DEV__ ? 'ws://localhost:80' : 'wss://chat-app-121.herokuapp.com';
-const AUTH_END_POINT = __DEV__ ? 'http://localhost:80/login' : 'http://chat-app-121.herokuapp.com/login';
+const AUTH_END_POINT = __DEV__ ? 'http://localhost:80/login' : 'https://chat-app-121.herokuapp.com/login';
 
 export { WSS_END_POINT, AUTH_END_POINT };
