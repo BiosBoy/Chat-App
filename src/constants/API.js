77194123b54533994c74c5f62de0793e0f6add03
Sviat Kuzhelev <30953392+BiosBoy@users.noreply.github.com>
2023-03-12 18:28:37 +0100

    Updated port

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,5 +1,5 @@
-const WSS_END_POINT = __DEV__ ? 'ws://localhost:3002' : 'wss://carradar.com.ua:5137';
-const JOIN_END_POINT = __DEV__ ? 'http://localhost:3002/join' : 'https://carradar.com.ua:5137/join';
-const AUTH_END_POINT = __DEV__ ? 'http://localhost:3002/auth' : 'https://carradar.com.ua:5137/auth';
+const WSS_END_POINT = __DEV__ ? 'ws://localhost:3002' : 'wss://carradar.com.ua:8443';
+const JOIN_END_POINT = __DEV__ ? 'http://localhost:3002/join' : 'https://carradar.com.ua:8443/join';
+const AUTH_END_POINT = __DEV__ ? 'http://localhost:3002/auth' : 'https://carradar.com.ua:8443/auth';
 
 export { WSS_END_POINT, JOIN_END_POINT, AUTH_END_POINT };
