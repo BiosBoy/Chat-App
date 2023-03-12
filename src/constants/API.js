ef4d70b297a5b9aedd45081b5fd42ad8b78e8dda
Sviat Kuzhelev <30953392+BiosBoy@users.noreply.github.com>
2023-03-12 21:29:04 +0100

    Updated roots

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,5 +1,5 @@
 const WSS_END_POINT = __DEV__ ? 'ws://localhost:3002' : 'wss://carradar.com.ua:8443';
-const JOIN_END_POINT = __DEV__ ? 'http://localhost:3002/join' : 'https://carradar.com.ua:8443/join';
-const AUTH_END_POINT = __DEV__ ? 'http://localhost:3002/auth' : 'https://carradar.com.ua:8443/auth';
+const JOIN_END_POINT = __DEV__ ? 'http://localhost:3002/join' : 'https://chat.carradar.com.ua/join';
+const AUTH_END_POINT = __DEV__ ? 'http://localhost:3002/auth' : 'https://chat.carradar.com.ua/auth';
 
 export { WSS_END_POINT, JOIN_END_POINT, AUTH_END_POINT };
